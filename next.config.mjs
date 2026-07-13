22e2324d58ab58428e82a2291388e1cfed2a3563
MatiasW380 <matiasbaronetto@gmail.com>
2026-07-13 19:43:07 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,1 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // La configuración de imágenes no la usaremos por ahora, pero la dejamos para futuras necesidades.
+  images: {
+    domains: [],
+  },
+};
 
+export default nextConfig;
